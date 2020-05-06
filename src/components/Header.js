32d1171b93b64324f290e4619a8829dffb93a4c3
analiseburtet <analiseburtet@gmail.com>
2020-05-06 16:33:22 -0300

    iniciar ui do componente header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 import { Container, Title, LogoImage } from './MyComponents'
 
 const Header = () => {
     return (
         <Container>
+            <Image
+                style={styles.logo}
+                source={require('../../assets/logo.png')}
+            />
             <Title>TODO LIST</Title>
         </Container>
     )
 }
 
 export default Header
+
+const styles = StyleSheet.create({
+    logo: {
+        height: 45,
+        width: 45
+    }
+})
