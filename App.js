d6561374627e6d21383685af515d9d1f87656f8a
Leonardo Felix <felix@orda-app.com>
2020-05-11 17:07:36 +0200

    feat: task CRUD handling

--- App.js
+++ App.js
@@ -1,41 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, AsyncStorage } from 'react-native'
-import { Container } from './src/components/ComponentsLib'
-import Header from './src/components/Header'
-import CreateTask from './src/components/CreateTask'
-import TaskList from './src/components/TaskList'
-import DoneList from './src/components/DoneList'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, AsyncStorage } from "react-native";
+import { Container } from "./src/components/ComponentsLib";
+import Header from "./src/components/Header";
+import CreateTask from "./src/components/CreateTask";
+import TaskList from "./src/components/TaskList";
 
 export default function App() {
-  const [ data, setData ] = useState([]);
-  
+  const [data, setData] = useState([]);
+
   const saveTask = async (id, value) => {
-    const unixTimestamp = id
-    const finalValues = JSON.stringify({...value})
-    await AsyncStorage.setItem(unixTimestamp, finalValues);
-    setData([...data, finalValues]);
-  };  
+    const unixTimestamp = id;
+    await AsyncStorage.setItem(
+      unixTimestamp,
+      JSON.stringify({ value, done: false })
+    );
+    setData([...data, { key: id, value, done: false }]);
+  };
 
   useEffect(() => {
-      const getData = async () => {
-      const retrievedData = (await displayData()).map(([, value]) =>
-          JSON.parse(value)
-      );
-          if (retrievedData && retrievedData.length) {
-              setData(retrievedData);
-          }
-      };
-      getData();
+    const getData = async () => {
+      let data = null;
+      try {
+        data = (await displayData()).map(([key, value]) => {
+          const finalValue = JSON.parse(value);
+          return { key, ...finalValue };
+        });
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+        data = [];
+      }
+      setData(data);
+    };
+    getData();
   }, []);
 
-  return(
+  const deleteTask = async (key) => {
+    try {
+      await AsyncStorage.removeItem(key);
+      const filteredData = data.filter((task) => task.key !== key);
+      setData(filteredData);
+    } catch (error) {
+      console.log(error, "imposible to delete item");
+    }
+  };
+
+  const onDoneToggle = async ({ key, done, value }) => {
+    await AsyncStorage.setItem(
+      key,
+      JSON.stringify({
+        done: !done,
+        value,
+      })
+    );
+    const newList = data.map((myTask) =>
+      myTask.key === key
+        ? {
+            ...myTask,
+            done: !myTask.done,
+          }
+        : myTask
+    );
+    setData(newList);
+  };
+
+  return (
     <Container main>
-      <Header/>
-      <CreateTask saveTask={saveTask}/>
-      <TaskList data={data}/>
-      <DoneList/>   
+      <Header />
+      <CreateTask saveTask={saveTask} />
+      {/* open tasks */}
+      <TaskList
+        data={data.filter((task) => !task.done)}
+        onDelete={deleteTask}
+        tag="OPEN TASKS"
+        onDoneToggle={onDoneToggle}
+      />
+      <TaskList
+        data={data.filter((task) => !!task.done)}
+        onDelete={deleteTask}
+        tag="DONE TASKS"
+        onDoneToggle={onDoneToggle}
+      />
     </Container>
-  )
+  );
 }
 
 const displayData = async () => {
@@ -44,7 +91,9 @@ const displayData = async () => {
     let keys = null;
     try {
       keys = await AsyncStorage.getAllKeys();
-      objects = await AsyncStorage.multiGet(keys);
+      objects = await AsyncStorage.multiGet(
+        keys.filter((key) => !!parseInt(key, 10))
+      );
       return objects;
     } catch (error) {
       console.log(error);
